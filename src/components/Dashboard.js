acd955b67d4461f39b9d8e4a433d840cda5af783
bbjarvis <brettbjarvis@gmail.com>
2021-12-23 18:59:18 -0800

    add scheduler-dashboard Loading and Panel funx

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,60 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
 
 class Dashboard extends Component {
+  state = {
+    loading: false
+  }
+
   render() {
+    const data = [
+      {
+        id: 1,
+        label: "Total Interviews",
+        value: 6
+      },
+      {
+        id: 2,
+        label: "Least Popular Time Slot",
+        value: "1pm"
+      },
+      {
+        id: 3,
+        label: "Most Popular Day",
+        value: "Wednesday"
+      },
+      {
+        id: 4,
+        label: "Interviews Per Day",
+        value: "2.3"
+      }
+    ];
+    const panelData = data.map(panels => {
+      
+      return(
+        <Panel
+          key={panels.id}
+          id={panels.id}
+          label={panels.label}
+          value={panels.value}
+        />
+      )
+    })
     const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return (
+    <main className={dashboardClasses}>
+      {panelData}
+
+    </main>
+    )
   }
 }
 
